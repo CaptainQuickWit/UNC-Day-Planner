a209ee09bd2fd63974559229d3c1bf4a66166e81
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-13 00:17:01 -0400

    got the html down. also added an aside which is where user will be given option to edit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(render("start"));
+
+var date = moment().format("MMM Do YY");
+
+function render(action) {
+    switch(action) {
+        case "start":
+
+            //code
+        break;
+    }
+}
+
