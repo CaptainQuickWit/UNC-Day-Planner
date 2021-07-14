48109ce39305f9c67c40c6531bb3614173acb862
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-14 03:33:08 -0400

    cleaned up some javascript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(start);
 var textAreaArray = $('textarea').get();
 var date;
@@ -6,6 +5,9 @@ var noteIdArray = [];
 var time = new Date().getHours();
 //var textAreaArray = document.getElementsByTagName("textarea");
 
+/**
+ * runs when page initially loads loads all info and runs moment.js
+ */
 function start() {
     date = moment().format('MMMM Do YYYY, h:mm:ss a');
             //code
@@ -13,14 +15,10 @@ function start() {
             time = 10;
             renderColor();
             storage("fetch");
-
 }
-$(".saveBtn").on("click", function (event) {
-    event.preventDefault();
-    currentEle = $(this);
-    storage("save",currentEle);
-});
-
+/**
+ * either add's or retrieves data from storage, both options are run as a single command
+ */
 function storage(action, element) {
     var index = 0;
     
@@ -29,17 +27,11 @@ function storage(action, element) {
         case "save":
             var textArea = $(element).siblings("textarea");
             var notes = textArea.val();
-
             var id = textArea.attr("id");
-            //var rowHourActive = $(this).siblings("textarea").data("number");
             window.localStorage.setItem(id, notes);
 
             console.log(id+"id");
-            console.log(notes+"notes");
-
-            //$(id).attr('saved','true')
-           
-          
+            console.log(notes+"notes");     
          break;
         
          case "fetch": //case fetch retrieves the info from local storage and 
@@ -51,46 +43,25 @@ function storage(action, element) {
                 var ele = $("textarea[data-index='" + i +"']");
                 noteIdArray[i] = $("textarea[data-index='" + i +"']").attr('id');
                 eleAttr = $("textarea[data-index='" + i +"']").attr('data-militaryTime');
-
-                renderColor(ele, eleAttr);
-
-
-                console.log(">>>>>>eleATTR>>>>>"+eleAttr+">>>>time>>>>"+time);
-                //array = $("textarea[data-militaryTime]);
-                console.log(noteIdArray[i]+"<<idarray");
-                console.log(i+"<<i for index");
-                //getAttribute('data-type')
-
+                renderColor(ele, eleAttr); //chooses the color while retriving info from the localstorage
                 id = noteIdArray[i];
                 
                 if (localStorage.getItem(id)) {
-                    $("textarea[data-index='" + i +"']").text(localStorage.getItem(id));
-                    
-                    console.log(localStorage.getItem(id)+"<<-======localstoragegetitem");
+                    $("textarea[data-index='" + i +"']").text(localStorage.getItem(id)); //loads text into element
                     
                 }
             }
-
 
         break;
     }
 }
 
-function getData(){
-    for (var i = 0; i < localStorage.length; i++) {
-      var key = localStorage.key(i);
-      textAreaArray.forEach(function(ele) {
-        if (ele.dataset.number == key) {
-            ele.value = localStorage.getItem(key)
-        }
-      })   
-    }
-}
-
-
+/**
+ * 
+ * @param {target element that we will manipulate} ele 
+ * @param {this is the ele data-militaryTime attribute} eleAttr 
+ */
 function renderColor(ele, eleAttr){
-
-    
     if (eleAttr == time) {
         ele.addClass("present");
     }
@@ -102,5 +73,14 @@ function renderColor(ele, eleAttr){
     }  
     
 }
+
+/**
+ * event listener for button
+ */
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    currentEle = $(this);
+    storage("save",currentEle);
+});
 
 
